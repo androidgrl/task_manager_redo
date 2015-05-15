1ca8dcee3cdfe8ec444c787483a36187fc4b2486
androidgrl <kawaharajamie@gmail.com>
2015-05-15 17:17:39 -0600

    Adding index, show, pages

---
+++ app/models/task_manager.rb
@@ -0,0 +1,39 @@
+require 'yaml/store'
+require_relative 'task'
+
+class TaskManager
+  def self.database
+    @database ||= YAML::Store.new("db/task_manager")
+  end
+
+  def self.create(task)
+    database.transaction do
+      database['tasks'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['tasks'] << { "id" => database['total'],
+                             "title" => task['title'],
+                             "description" => task['description'] }
+    end
+  end
+
+  def self.raw_tasks
+    database.transaction do
+      database['tasks'] || []
+    end
+  end
+
+  def self.all
+    raw_tasks.map do |data|
+      Task.new(data)
+    end
+  end
+
+  def self.raw_task(id)
+    raw_tasks.find { |task| task["id"] == id }
+  end
+
+  def self.find(id)
+    Task.new(raw_task(id))
+  end
+end
